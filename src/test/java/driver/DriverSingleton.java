dbd0482ddefda37921c3b49fbf6a23d72e74fc2f
Bekh Andrey <andreyac17@gmail.com>
2020-12-22 16:32:41 +0300

    opera driver added

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class DriverSingleton {
 
@@ -18,9 +19,13 @@ public class DriverSingleton {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                 }
-                default: {
+                case "chrome": {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                }
+                default: {
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
                 }
             }
             driver.manage().window().maximize();
